df0d66d1a10692cd165d825b58da2025e40a4ffb
sadegh-dev <sadegh.rez@gmail.com>
2024-03-10 23:17:45 +0330

    fresh start

--- weblog/fblog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FblogConfig(AppConfig):
-    name = 'fblog'
--- weblog/fblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('add-post/', views.addpost, name='add-post'),
-]
-
-
