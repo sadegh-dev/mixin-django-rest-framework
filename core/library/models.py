c9adb116ff0716c8ae1913ad0eb504762a82f263
sadegh-dev <sadegh.rez@gmail.com>
2024-03-11 21:56:00 +0330

    create Book and Category and Author classModel

--- core/library/models.py
+++ core/library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Book(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Author(models.Model):
+    pass
